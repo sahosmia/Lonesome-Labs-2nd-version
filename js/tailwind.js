d8c31934ff31ef75167080db16d6125ad6195660
sahosridoy <sahosmia.webdev@gmail.com>
2023-12-20 00:11:17 +0600

    shift cdn to cli

---
+++ js/tailwind.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        main: "#3A0CA3",
+        secondary: "#4361EE",
+      },
+
+      backgroundImage: {
+        "hero-pattern": "url('/img/hero-bg.png')",
+      },
+    },
+  },
+};
